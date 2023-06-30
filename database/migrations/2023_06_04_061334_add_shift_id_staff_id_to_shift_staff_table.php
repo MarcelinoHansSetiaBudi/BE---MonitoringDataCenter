9e9ca082b0dbe4db1c4693384d68c32f429f81a4
Tjoa <100271617+MarcelinoHansSetiaBudi@users.noreply.github.com>
2023-06-30 20:30:24 +0700

    fixed bug at FK

--- database/migrations/2023_06_04_061334_add_shift_id_staff_id_to_shift_staff_table.php
+++ database/migrations/2023_06_04_061334_add_shift_id_staff_id_to_shift_staff_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::table('shift_staff', function (Blueprint $table) {
             $table->unsignedBigInteger('staff_id')->after('id')->nullable();
-            $table->foreign('staff_id')->references('id')->on('data_staff')->onDelete('restrict');
+            $table->foreign('staff_id')->references('id')->on('data_staff');
             $table->unsignedBigInteger('shift_id')->after('staff_id')->nullable();
-            $table->foreign('shift_id')->references('id')->on('shift')->onDelete('restrict');
+            $table->foreign('shift_id')->references('id')->on('shift');
         });
     }
 
